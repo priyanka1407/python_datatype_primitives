c7983f22705fd2b1df202ad1fcaec69fd9492d4d
Priyanka0706 <priyankamurugavel@gmail.com>
2021-10-28 05:10:56 +0000

    Initial commit

---
+++ datatypes_typecasting.py
@@ -0,0 +1,44 @@
+#print the length of the string value,
+#if we print the length of integer it throws Error
+print(len("93809"))
+#len(1234)
+#Data Types
+#strings
+print("Hello"[4])
+print("123"[1]+"345"[2])
+#Integers: should be declared without double quotes
+print(123_345_678)
+print(1234567)
+#Float
+print(3567.89)
+print(6.78093)
+#Boolean
+print(True)
+print(False)
+
+######################################
+
+#converting integer to string, string to float, float to integer by type casting
+
+num_char=len(input("what is your name?"))
+new_num_char=str(num_char)
+print("my name has "+ new_num_char+ " characters")
+
+#type is the method for determining the data type
+#if its str,loat, integer
+a= float(123)
+print(type(a))
+
+print(123+float(8963.90))
+
+print(str(726)+str(78528.98))
+
+
+#challenge
+
+
+
+
+
+
+
